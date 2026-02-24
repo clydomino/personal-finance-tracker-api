8beef27185c0f49a83ff73c27e94dd9dbd83c838
clydomino <clydomino@gmail.com>
2026-02-24 11:08:20 -0500

    Added application classes for CreateUser

---
+++ api/FinanceTracker.Application/Users/CreateUser/CreateUserHandler.cs
@@ -0,0 +1,53 @@
+﻿using FinanceTracker.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Application.Users.CreateUser
+{
+    public class CreateUserHandler : IRequestHandler<CreateUserCommand, CreateUserResult>
+    {
+
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public CreateUserHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+
+
+        public async Task<CreateUserResult> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            // Check if email already exists
+            var existingUser = await _userRepository.GetByEmailAsync(request.Email, cancellationToken);
+
+            if (existingUser is not null)
+            {
+                throw new Exception("User with this email already exists.");
+            }
+
+            // Create domain entity
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+            };
+
+            //Hash password
+            user.PasswordHash = _passwordHasher.HashPassword(user, request.Password);
+
+            // Persist user
+            await _userRepository.AddAsync(user, cancellationToken);
+
+            // Return result DTO
+            return new CreateUserResult(user.Id, user.Email, user.FirstName, user.LastName);
+        }
+    }
+}
