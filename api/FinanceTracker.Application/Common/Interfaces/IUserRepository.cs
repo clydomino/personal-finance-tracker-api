28dd6001c2566cc0c55642abe7b6721d75957d28
clydomino <clydomino@gmail.com>
2026-02-26 12:19:55 -0500

    Moved IUserRepository into Application Layer

---
+++ api/FinanceTracker.Application/Common/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Domain
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken);
+        Task AddAsync(User user, CancellationToken cancellationToken);
+    }
+}
