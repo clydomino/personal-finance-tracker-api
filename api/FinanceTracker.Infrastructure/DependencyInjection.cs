8736086e3402f31fb18289660440a752a1ccbb17
clydomino <clydomino@gmail.com>
2026-03-09 19:31:45 -0400

    Inject PasswordHasher

--- api/FinanceTracker.Infrastructure/DependencyInjection.cs
+++ api/FinanceTracker.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,10 @@
 ﻿using FinanceTracker.Domain;
 using FinanceTracker.Infrastructure.Persistence.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace FinanceTracker.Infrastructure
 {
@@ -17,6 +16,7 @@ namespace FinanceTracker.Infrastructure
             options.UseSqlServer(configuration.GetConnectionString("Default")));
 
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             
             return services;
         }
