4d3c5c4e67a569d22ebc791b4338a8079abd8ce5
clydomino <clydomino@gmail.com>
2026-03-09 19:30:50 -0400

    Inject MediatR

--- api/FinanceTracker.Api/Program.cs
+++ api/FinanceTracker.Api/Program.cs
@@ -1,3 +1,4 @@
+using FinanceTracker.Application.Users.CreateUser;
 using FinanceTracker.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly((typeof(CreateUserCommand)).Assembly));
 
 var app = builder.Build();
 
