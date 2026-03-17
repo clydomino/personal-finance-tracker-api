2177fd227aaa5a6e2c3935e53f5dd241c29af32a
clydomino <clydomino@gmail.com>
2026-03-17 15:17:05 -0400

    Add login feature logic

---
+++ api/FinanceTracker.Application/Auth/Login/LoginHandler.cs
@@ -0,0 +1,44 @@
+﻿using FinanceTracker.Application.Common.Interfaces;
+using FinanceTracker.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Application.Auth.Login
+{
+    public class LoginHandler : IRequestHandler<LoginCommand, LoginResult>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public LoginHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher, IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+
+        public async Task<LoginResult> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.Email, cancellationToken);
+
+            if (user == null) {
+                return new LoginResult(false, "Invalid creadentials", null);
+            }
+
+            var passwordVerificationResult = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, request.Password);
+
+            if (passwordVerificationResult == PasswordVerificationResult.Failed) {
+                return new LoginResult(false, "Invalid creadentials", null);
+            }
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new LoginResult(true, null, token);
+        }
+    }
+}
