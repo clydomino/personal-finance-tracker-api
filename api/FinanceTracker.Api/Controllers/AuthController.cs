49b53ac42f1f8cc1454292dc0b0bd2d51842e448
clydomino <clydomino@gmail.com>
2026-03-17 15:19:19 -0400

    Add login endpoint

---
+++ api/FinanceTracker.Api/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using FinanceTracker.Application.Auth.Login;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTracker.Api.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.Success)
+            {
+                return Unauthorized(result.Error);
+            }
+
+            return Ok(result);
+        }
+    }
+}
