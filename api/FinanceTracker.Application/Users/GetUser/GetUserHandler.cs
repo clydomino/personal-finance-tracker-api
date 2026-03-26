98a0d6332b64f08589844f59a1614353872bb1e1
clydomino <clydomino@gmail.com>
2026-03-25 23:05:17 -0400

    Add GetUserHandler

---
+++ api/FinanceTracker.Application/Users/GetUser/GetUserHandler.cs
@@ -0,0 +1,27 @@
+﻿using FinanceTracker.Domain;
+using MediatR;
+
+namespace FinanceTracker.Application.Users.GetUser
+{
+    public class GetUserHandler : IRequestHandler<GetUserQuery, GetUserResult>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GetUserResult> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id, cancellationToken);
+
+            if (user == null)
+            {
+                return new GetUserResult(false, "User not found", null, null, null, null);
+            }
+
+            return new GetUserResult(true, null, user.Id, user.Email, user?.FirstName, user?.LastName);
+        }
+    }
+}
