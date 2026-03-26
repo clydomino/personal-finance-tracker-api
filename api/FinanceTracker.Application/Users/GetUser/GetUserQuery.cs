d5e5f4411c8901578cec9bf9bd31cacf92f384b2
clydomino <clydomino@gmail.com>
2026-03-25 23:05:34 -0400

    Add GetUserQuery record

---
+++ api/FinanceTracker.Application/Users/GetUser/GetUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Application.Users.GetUser
+{
+    public record GetUserQuery(Guid Id)
+    : IRequest<GetUserResult>;
+}
