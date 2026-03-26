9c4f400dada50b93ac8f8bdad6ed0ea083c97601
clydomino <clydomino@gmail.com>
2026-03-25 23:04:37 -0400

    Add GetUserResult record

---
+++ api/FinanceTracker.Application/Users/GetUser/GetUserResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Application.Users.GetUser
+{
+    public record GetUserResult(
+        bool Success,
+        string? Error,
+        Guid? Id,
+        string? Email,
+        string? FirstName,
+        string? LastName
+ );
+}
